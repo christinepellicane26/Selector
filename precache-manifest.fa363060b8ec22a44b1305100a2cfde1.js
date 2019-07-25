d4c7270aca264e5e75b77d8274c43ec971ff5908
christinepellicane26 <christinepellicane@gmail.com>
2019-07-25 15:38:05 -0400

    Updates

---
+++ precache-manifest.fa363060b8ec22a44b1305100a2cfde1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af1e5cc4e0aee0b7b06952f5519e35df",
+    "url": "/selector/index.html"
+  },
+  {
+    "revision": "8c1ce6b46c6ba9a278ac",
+    "url": "/selector/static/css/main.4dc763a6.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/selector/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "8c1ce6b46c6ba9a278ac",
+    "url": "/selector/static/js/main.e95858ec.chunk.js"
+  },
+  {
+    "revision": "90afcdfb8efb11a1f025",
+    "url": "/selector/static/js/runtime~main.3573b4bb.js"
+  }
+]);
